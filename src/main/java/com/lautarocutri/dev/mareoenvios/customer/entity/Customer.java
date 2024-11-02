68b59bec012469a0a12a203ce3331d637acc9a65
LautaroC <lautarocutri@gmail.com>
2024-11-02 17:05:56 -0300

    feat(ME-1): Remove unneeded factory method

--- src/main/java/com/lautarocutri/dev/mareoenvios/customer/entity/Customer.java
+++ src/main/java/com/lautarocutri/dev/mareoenvios/customer/entity/Customer.java
@@ -1,12 +1,18 @@
 package com.lautarocutri.dev.mareoenvios.customer.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import com.lautarocutri.dev.mareoenvios.database.AuditedEntity;
 import com.lautarocutri.dev.mareoenvios.database.DBConst;
+import com.lautarocutri.dev.mareoenvios.shipping.entity.ShippingItem;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -40,13 +46,4 @@ public class Customer extends AuditedEntity {
 	@NonNull
 	@Column(nullable = false, length = DBConst.DB_STRING_LENGTH)
 	private String city;
-
-	public static Customer createNew(
-			String firstName,
-			String lastName,
-			String address,
-			String city
-	) {
-		return new Customer(firstName, lastName, address, city);
-	}
 }
