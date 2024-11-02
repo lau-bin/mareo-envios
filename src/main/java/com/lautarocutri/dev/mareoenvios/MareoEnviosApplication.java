e02e3c214f76beea01ffb72e99dd4e0e2fd1fd72
LautaroC <lautarocutri@gmail.com>
2024-11-02 00:14:44 -0300

    feat(ME-1): Main app class

---
+++ src/main/java/com/lautarocutri/dev/mareoenvios/MareoEnviosApplication.java
@@ -0,0 +1,13 @@
+package com.lautarocutri.dev.mareoenvios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MareoEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MareoEnviosApplication.class, args);
+	}
+
+}
