2d171d0ddd7b047ff62d49decde5bd07f9da15b0
LautaroC <lautarocutri@gmail.com>
2024-11-02 00:27:00 -0300

    feat(ME-1): Minor Refactoring

--- src/main/java/com/lautarocutri/dev/mareoenvios/auth/AuthUser.java
+++ src/main/java/com/lautarocutri/dev/mareoenvios/auth/AuthUser.java
@@ -48,6 +48,6 @@ public class AuthUser {
 
 	public enum Role {
 		ADMIN,
-		USER;
+		USER
 	}
 }
--- src/main/java/com/lautarocutri/dev/mareoenvios/database/DBConst.java
+++ src/main/java/com/lautarocutri/dev/mareoenvios/database/DBConst.java
@@ -1,7 +1,10 @@
 package com.lautarocutri.dev.mareoenvios.database;
 
-public class DBConst {
+public final class DBConst {
 	public static final int DB_STRING_LENGTH = 255;
 	public static final int DB_ENUM_LENGTH = 50;
 	public static final int DB_UUID_LENGTH = 36;
+
+	private DBConst() {
+	}
 }
